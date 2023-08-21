fc08810b95be3bd0fc10f3f9ab2ad06a088310f9
chaitananjfwfuiwbufbrui <59876819+chaitananjfwfuiwbufbrui@users.noreply.github.com>
2023-08-21 06:57:55 +0530

    booking models added

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Saloon, Service, Portfolio, Time_slot
 from .serializers import SaloonSerializer, SaloonSIngle_Serializer, ServiceSerializer, PortfolioSerializer, Time_slotSerializer
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser  # Add this import line
+from styln.settings import AUTH_USER_MODEL
 class SaloonListCreateView(generics.ListCreateAPIView):
     queryset = Saloon.objects.all()
     serializer_class = SaloonSerializer
@@ -43,3 +44,27 @@ class TimeSlotAPI(APIView):
         time_slots = Time_slot.objects.filter(saloon=saloon)
         serializer = Time_slotSerializer(time_slots, many=True)
         return Response(serializer.data)
+class Lcation_taker(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = (MultiPartParser, FormParser, FileUploadParser)
+
+    def get(self, request, id):
+        user = request.user
+        user.latitude = request.get.data('latitude')
+        user.longitude = request.get.data('longitude')
+        user.save()
+
+        message = {"message": "location  saved"}
+        return JsonResponse(message, status=status.HTTP_200_OK)
+class Lcation_taker(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = (MultiPartParser, FormParser, FileUploadParser)
+
+    def get(self, request, id):
+        user = request.user
+        user.latitude = request.get.data('latitude')
+        user.longitude = request.get.data('longitude')
+        user.save()
+
+        message = {"message": "location  saved"}
+        return JsonResponse(message, status=status.HTTP_200_OK)
